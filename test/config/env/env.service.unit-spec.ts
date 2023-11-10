3ce8cbf45eb729fbd25f5b3aa8dcd12e2fb8fbce
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-11-10 17:43:48 -0300

    test: ensure EnvService returns the correct port number on success

--- test/config/env/env.service.unit-spec.ts
+++ test/config/env/env.service.unit-spec.ts
@@ -1,6 +1,7 @@
 import { Test, type TestingModule } from '@nestjs/testing'
 import { EnvService } from '../../../src/config/env/env.service'
 import { EnvModule } from '../../../src/config/env/env.module'
+import { ConfigService } from '@nestjs/config'
 
 describe('EnvService', () => {
   let sut: EnvService
@@ -8,7 +9,7 @@ describe('EnvService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [EnvModule.forRoot()],
-      providers: [EnvService]
+      providers: [EnvService, ConfigService]
     }).compile()
 
     sut = module.get<EnvService>(EnvService)
@@ -16,5 +17,11 @@ describe('EnvService', () => {
 
   it('should be defined', () => {
     expect(sut).toBeDefined()
+  })
+
+  it('should return the correct port number on success', () => {
+    const port = sut.getAppPort()
+
+    expect(port).toBe(3000)
   })
 })
