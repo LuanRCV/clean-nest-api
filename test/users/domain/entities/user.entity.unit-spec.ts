86d09d07b19ab406367258c796c65c23baec0bc4
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-11-11 11:28:52 -0300

    refactor: add userParams type

--- test/users/domain/entities/user.entity.unit-spec.ts
+++ test/users/domain/entities/user.entity.unit-spec.ts
@@ -1,10 +1,10 @@
-import { UserEntity } from '@/users/domain/entities/user.entity'
+import { UserEntity, type UserParams } from '@/users/domain/entities/user.entity'
 import { faker } from '@faker-js/faker'
 
 describe('UserEntity', () => {
   describe('Constructor', () => {
     test('should create a UserEntity with correct values on success', () => {
-      const userParams = {
+      const userParams: UserParams = {
         name: faker.person.fullName(),
         email: faker.internet.email(),
         password: faker.internet.password()
