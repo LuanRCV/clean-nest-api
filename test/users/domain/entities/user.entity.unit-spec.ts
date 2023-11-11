d1a9500bccafb8e5a8c626e6a502cdfaef010469
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-11-11 11:35:31 -0300

    refactor: add sut creation to a separated method

--- test/users/domain/entities/user.entity.unit-spec.ts
+++ test/users/domain/entities/user.entity.unit-spec.ts
@@ -2,15 +2,20 @@ import { UserEntity, type UserParams } from '@/users/domain/entities/user.entity
 import { faker } from '@faker-js/faker'
 
 describe('UserEntity', () => {
+  let userParams: UserParams
+  let sut: UserEntity
+
+  beforeEach(() => {
+    userParams = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
+    sut = new UserEntity(userParams)
+  })
+
   describe('Constructor', () => {
     test('should create a UserEntity with correct values on success', () => {
-      const userParams: UserParams = {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password()
-      }
-      const sut = new UserEntity(userParams)
-
       expect(sut.data).toEqual(userParams)
     })
   })
