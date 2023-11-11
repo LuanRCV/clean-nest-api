98dca7954789886454ca8ef3b8211076eb51b06c
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-11-11 09:45:31 -0300

    feat: add users resource

---
+++ src/users/users.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common'
+import { type CreateUserDto } from './dto/create-user.dto'
+import { type UpdateUserDto } from './dto/update-user.dto'
+
+@Injectable()
+export class UsersService {
+  create (createUserDto: CreateUserDto): string {
+    return 'This action adds a new user'
+  }
+
+  findAll (): string {
+    return 'This action returns all users'
+  }
+
+  findOne (id: number): string {
+    return `This action returns a #${id} user`
+  }
+
+  update (id: number, updateUserDto: UpdateUserDto): string {
+    return `This action updates a #${id} user`
+  }
+
+  remove (id: number): string {
+    return `This action removes a #${id} user`
+  }
+}
