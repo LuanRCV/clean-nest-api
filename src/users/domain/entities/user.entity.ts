5b4141614ac7225114f60c0747fd0647612dfe4b
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-11-11 11:25:57 -0300

    feat: add UserEntity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,14 @@
-export class UserEntity {}
+export type UserParams = {
+  name: string
+  email: string
+  password: string
+  createdAt?: Date
+}
+
+export class UserEntity {
+  constructor (
+    public readonly data: UserParams
+  ) {
+    this.data.createdAt = this.data.createdAt ?? new Date()
+  }
+}
