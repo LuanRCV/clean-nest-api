b513fc2aac3f5ce7f5568ca03a208d11e57f0c4b
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-11-11 14:12:41 -0300

    feat: add id into mockUserProps

--- test/users/mocks/user-props.mock.ts
+++ test/users/mocks/user-props.mock.ts
@@ -3,6 +3,7 @@ import { faker } from '@faker-js/faker'
 
 export function mockUserProps (): UserProps {
   return {
+    id: faker.string.uuid(),
     name: faker.person.fullName(),
     email: faker.internet.email(),
     password: faker.internet.password()
