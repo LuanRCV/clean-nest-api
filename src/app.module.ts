9ac043063ecc011455bc915fcb86f47a46f90b19
LuanRCV <luan.rocha2014@engenharia.ufjf.br>
2023-11-11 09:49:19 -0300

    refactor: move users resource to a clean arch approach folders

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { EnvModule } from './config/env/env.module'
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users/infrastructure/users.module'
 
 @Module({
   imports: [EnvModule, UsersModule],
